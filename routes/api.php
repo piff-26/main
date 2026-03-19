ac2d238ba857b51c3b1dd4362ea21ae9055c3c35
natanieljoshe <natanieljoshe@gmail.com>
2026-03-19 08:57:30 +0700

    feat: create checkin api point

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\TicketController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/midtrans/callback', [PaymentController::class, 'callback']);
+
+Route::get('/checkin/{ticket_code}', [TicketController::class, 'processCheckIn'])->name('api.checkin');
