1e508f6e527092376fa7890411bcb3d6d84c9bdb
natanieljoshe <natanieljoshe@gmail.com>
2026-03-20 09:33:30 +0700

    chore: add img, desc, and close sell time column

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,8 +9,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = ['name', 'slug', 'location', 'event_date', 'start_time', 'end_time'];
-    protected $casts = ['event_date' => 'date', 'start_time' => 'datetime:H:i', 'end_time' => 'datetime:H:i'];
+    protected $fillable = ['name', 'slug', 'location', 'event_date', 'start_time', 'end_time', 'image', 'description', 'close_sell_time'];
+    protected $casts = ['event_date' => 'date', 'start_time' => 'datetime:H:i', 'end_time' => 'datetime:H:i', 'close_sell_time' => 'datetime'];
 
     public function ticketCategories() {
         return $this->hasMany(TicketCategory::class);
