26b674bd4935df97bf390e6a4bbb4526ba9eecbd
natanieljoshe <natanieljoshe@gmail.com>
2026-03-20 16:07:48 +0700

    chore: delete crsf check

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,9 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => AdminMiddleware::class,
             'auth' => AuthMiddleware::class,
         ]);
-        $middleware->validateCsrfTokens(except: [
-            'midtrans/callback',
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
