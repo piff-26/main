6f18e53ee20dbe67cca5260d3c8b4de9d07d4a2e
natanieljoshe <natanieljoshe@gmail.com>
2026-05-27 11:18:33 +0700

    chore: add backpass email for view online movie

---
+++ app/Enums/AllowedOnlineEventEmails.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+class AllowedOnlineEventEmails
+{
+    public const LIST = [
+        'piff.pcu@gmail.com',
+        'c14240092@john.petra.ac.id',
+        'natanieljoshe11@gmail.com'
+    ];
+}
